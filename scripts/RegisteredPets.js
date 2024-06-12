0bb6349fe85fba435bf87a9e1bd6962ab5e8c227
Abdul Alsafri <abdulalsafri1@gmail.com>
2024-06-12 09:40:18 -0500

    When a pet is clicked, the walker is shown

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,4 +1,19 @@
-import { getPets } from "./database.js";
+import { getPets, getWalkers } from "./database.js";
+
+document.addEventListener("click", (clickEvent) => {
+  const clickTarget = clickEvent.target;
+
+  const walkerId = clickTarget.dataset.walkerforeignkey;
+
+  // Find the entire walker obj based on walkerId from above
+
+  const allWalkers = getWalkers();
+  for (const walker of allWalkers) {
+    if (walker.id === parseInt(walkerId)) {
+      window.alert(`This pet is being walked by ${walker.name}`);
+    }
+  }
+});
 
 export const RegisteredPets = () => {
   const pets = getPets();
@@ -6,7 +21,7 @@ export const RegisteredPets = () => {
   let petsHTML = "<ul>";
 
   for (const pet of pets) {
-    petsHTML += `<li data-walkerForeignKey="${pet.walkerId}>${pet.name}</li>`;
+    petsHTML += `<li data-walkerForeignKey="${pet.walkerId}">${pet.name}</li>`;
   }
 
   petsHTML += "</ul>";
