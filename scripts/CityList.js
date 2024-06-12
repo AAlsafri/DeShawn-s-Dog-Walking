4954b48ffc558e1e132f91dda8d24769efa31be0
Abdul Alsafri <abdulalsafri1@gmail.com>
2024-06-12 10:25:26 -0500

    When a city is clicked, the walkers working in that city are displayed

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -1,12 +1,20 @@
 import { getWalkers } from "./database.js";
 
+const walkers = getWalkers();
+
+document.addEventListener("click", (clickEvent) => {
+  const cityTarget = clickEvent.target;
+
+  if (cityTarget.dataset.type === "city") {
+    window.alert(`${cityTarget.dataset.walkername} is servicing this city`);
+  }
+});
+
 export const CityList = () => {
-  const walkers = getWalkers();
-  // Assuming that each city should be listed once.
-  const cities = [...new Set(walkers.map((walker) => walker.city))];
   let citiesHTML = "<ul>";
-  for (const city of cities) {
-    citiesHTML += `<li>${city}</li>`;
+
+  for (const walker of walkers) {
+    citiesHTML += `<li data-type="city" data-walkername="${walker.name}">${walker.city}</li>`;
   }
   citiesHTML += "</ul>";
   return citiesHTML;
